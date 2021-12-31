021db8f116a20c226958d4b26c839a8d9951ce57
Amman Vedi <av@Ammans-MacBook-Pro.local>
2021-12-31 22:48:38 +0000

    make sure the build output calls a require on react and react dom

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
 const path = require('path');
 
+const reactExternal = {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'react',
+};
+const reactDOMExternal = {
+    root: 'ReactDOM',
+    commonjs2: 'react-dom',
+    commonjs: 'react-dom',
+    amd: 'react-dom',
+};
+
 module.exports = {
+    target: 'web',
     devtool: 'source-map',
-    externals: ['react', 'react-dom'],
+    externals: {
+        react: reactExternal,
+        'react-dom': reactDOMExternal,
+    },
     entry: './src/index.ts',
     mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
+        libraryTarget: 'commonjs',
     },
     module: {
         rules: [
